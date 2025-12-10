6c68cb8c0858278442af1782d8ff014a09defef0
Andrew Wittrock <andy@wittrock.us>
2025-12-09 23:54:01 -0600

    Reload links page

--- lib/routes.ts
+++ lib/routes.ts
@@ -1,9 +1,10 @@
+import { withReloadRoute } from "@core/lib/useReloadRoute";
 import { About } from "../components/About";
 import { Links } from "../components/Links";
 
 export const customRoutes = {
     public: [
         {path: "/team", component: About},
-        {path: "/links", component: Links}
+        {path: "/links", component: withReloadRoute(Links)}
     ]
 };
