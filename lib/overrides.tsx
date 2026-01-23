f604c50481974919dc1ffc98d79f86ce08a87905
Andrew Wittrock <andy@wittrock.us>
2026-01-22 22:49:43 -0600

    Remove unneeded overrides

--- lib/overrides.tsx
+++ lib/overrides.tsx
@@ -1,49 +1,16 @@
 import { ArchivesHeader } from "@comic/components/ArchivesHeader";
 import { CharacterPageHeader } from "@comic/components/CharactersPage/CharacterPageHeader";
 import { PageHeader } from "@common/components/Page/PageHeader";
-import { ShadowBackground } from "@darkwynd/components/ShadowBackground";
-import { ProductPage } from "@store/components/ProductPage";
-import { SearchResults } from "@store/components/ProductsPage/SearchResults";
 
-import { Cart } from "@store/components/Cart";
 import { LoginFormComponent } from "@uac/components/LoginForm/LoginForm.component";
-import { MyAccount } from "@uac/components/MyAccount";
 import loginFormStyles from "../components/LoginForm/LoginForm.module.scss";
 
 // Custom site banner
 LoginFormComponent.overrideStyles(loginFormStyles);
 
-// Custom fancy header and shadow background for the archives
+// Custom fancy header for the archives, CMS pages, and character page
 ArchivesHeader.hide();
 
 PageHeader.hide();
 
 CharacterPageHeader.hide();
-
-// ProductPage.override(({ Original }) => <>
-//     <div style={{ marginTop: "-20px" }}>
-//         <ShadowBackground hideLeftBottom hideRightBottom>
-//             <div style={{ marginTop: "-128px" }} >
-//                 <Original />
-//             </div>
-//         </ShadowBackground>
-//     </div>
-// </>);
-
-SearchResults.override(({ Original }) => <>
-    <div style={{ paddingBottom: "128px" }}>
-        <Original />
-    </div>
-</>);
-
-MyAccount.override(({ Original }) => <>
-    <div style={{ paddingBottom: "256px" }}>
-        <Original />
-    </div>
-</>);
-
-Cart.override(({ Original }) => <>
-    <div style={{ paddingBottom: "128px" }}>
-        <Original />
-    </div>
-</>);
