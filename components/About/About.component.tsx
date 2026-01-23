da5baf727c88e006725375aca9218d1d7999620f
Andrew Wittrock <andy@wittrock.us>
2026-01-22 23:23:23 -0600

    Remove shadow background from team page

--- components/About/About.component.tsx
+++ components/About/About.component.tsx
@@ -1,26 +1,23 @@
 import { Snippet } from "@common/components/Snippet";
 import { overridable } from "@core/lib/overridable";
-import { ShadowBackground } from "../ShadowBackground";
 import { AboutProps } from "./About.d";
 import styles from './About.module.scss';
 
 export const AboutComponent = overridable(({ credits, classes = styles }: AboutProps) => <>
-    <ShadowBackground>
-        <div className={classes.about}>
-            <div className={classes.credits}>
-                {credits.map(credit => <div key={credit.name} className={classes.credit}>
-                    <div className={classes.icon}>
-                        {credit.icon ? <img src={credit.icon} alt={credit.name} /> : <>&nbsp;&nbsp;{credit.name.charAt(0)}</>}
-                    </div>
-                    <h3>
-                        <div className={classes.role}>{credit.role}</div>
-                        <div className={classes.name}>{credit.name}</div>
-                    </h3>
-                    <p className={classes.info}>
-                        <Snippet slug={credit.slug} />
-                    </p>
-                </div>)}
-            </div>
+    <div className={classes.about}>
+        <div className={classes.credits}>
+            {credits.map(credit => <div key={credit.name} className={classes.credit}>
+                <div className={classes.icon}>
+                    {credit.icon ? <img src={credit.icon} alt={credit.name} /> : <>&nbsp;&nbsp;{credit.name.charAt(0)}</>}
+                </div>
+                <h3>
+                    <div className={classes.role}>{credit.role}</div>
+                    <div className={classes.name}>{credit.name}</div>
+                </h3>
+                <p className={classes.info}>
+                    <Snippet slug={credit.slug} />
+                </p>
+            </div>)}
         </div>
-    </ShadowBackground>
+    </div>
 </>);
