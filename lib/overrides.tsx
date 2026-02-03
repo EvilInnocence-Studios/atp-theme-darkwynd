c62f5d3d60a28f83bfb42d16d93e1120cc121744
Andrew Wittrock <andy@wittrock.us>
2026-02-02 21:36:34 -0600

    Remove commented code

--- lib/overrides.tsx
+++ lib/overrides.tsx
@@ -2,12 +2,6 @@ import { ArchivesHeader } from "@comic/components/ArchivesHeader";
 import { CharacterPageHeader } from "@comic/components/CharactersPage/CharacterPageHeader";
 import { PageHeader } from "@common/components/Page/PageHeader";
 
-import { LoginFormComponent } from "@uac/components/LoginForm/LoginForm.component";
-import loginFormStyles from "../components/LoginForm/LoginForm.module.scss";
-
-// Custom site banner
-// LoginFormComponent.overrideStyles(loginFormStyles);
-
 // Custom fancy header for the archives, CMS pages, and character page
 ArchivesHeader.hide();
 
