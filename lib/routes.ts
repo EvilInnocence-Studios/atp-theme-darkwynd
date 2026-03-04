a873272229bd7bcf98a8ac9540328de31f0edb08
Andrew Wittrock <andy@wittrock.us>
2026-03-03 23:26:44 -0600

    Remove refresh on links page

--- lib/routes.ts
+++ lib/routes.ts
@@ -1,10 +1,9 @@
-import { withReloadRoute } from "@core/lib/useReloadRoute";
 import { About } from "../components/About";
 import { Links } from "../components/Links";
 
 export const customRoutes = {
     public: [
-        {path: "/team", component: About},
-        {path: "/links", component: withReloadRoute(Links)}
+        { path: "/team", component: About },
+        { path: "/links", component: Links }
     ]
 };
