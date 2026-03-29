2db36da2166484b95afd63ec9688d80d40386691
Andrew Wittrock <andy@wittrock.us>
2026-03-28 22:45:04 -0500

    Remove archive header override

--- lib/overrides.tsx
+++ lib/overrides.tsx
@@ -1,9 +1,7 @@
-import { ArchivesHeader } from "@comic/components/ArchivesHeader";
 import { CharacterPageHeader } from "@comic/components/CharactersPage/CharacterPageHeader";
 import { PageHeader } from "@common/components/Page/PageHeader";
 
 // Custom fancy header for the archives, CMS pages, and character page
-ArchivesHeader.hide();
 
 PageHeader.hide();
 
